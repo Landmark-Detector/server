3e61b6ffbc5a6ea6b01a880a6855f13b81b10642
didadadida93 <didadadida93@gmail.com>
2020-01-17 16:09:39 +0700

    add morgan

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV === 'development') require('dotenv').config()
 
+const morgan = require('morgan')
 const cors = require('cors')
 const express = require('express')
 const app = express()
@@ -7,6 +8,7 @@ const app = express()
 require('./config/mongodb')
 
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/', require('./routes'))
